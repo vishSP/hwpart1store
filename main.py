f86f6429d7b2d61a5e133add9305fc63c67e7247
vishSP <hihi64741@gmail.com>
2023-02-14 15:32:47 +0200

    удалил мусор и добавил докстринг

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 class Item:
+    """размер скидки"""
     pay_rate = 0.85
-    all =[]
+    """Список для вывода экземпляров """
+    all = []
+
     def __init__(self, name, price, amount):
+        """инициализация имени, цены, количества"""
         self.name = name
         self.price = price
         self.amount = amount
@@ -11,23 +15,11 @@ class Item:
         self.total_price = 0
 
     def apply_discount(self):
-        self.price= self.price * self.pay_rate
+        """применение скидки"""
+        self.price = self.price * self.pay_rate
         return self.price
 
     def calculate_total_price(self):
+        """подсчет полной цены товара"""
         self.total_price = self.price * self.amount
         return self.total_price
-
-
-item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
-
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
-
-Item.pay_rate = 0.8  # устанавливаем новый уровень цен
-item1.apply_discount()
-print(item1.price)
-print(item2.price)
-
-print(Item.all)
