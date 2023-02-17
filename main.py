1cef60a1fcee112837223ee6e18e841ded14b16e
vishSP <hihi64741@gmail.com>
2023-02-17 18:50:17 +0200

    написал код, добавил докстринг и тесты

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import csv
+
 class Item:
     """размер скидки"""
     pay_rate = 0.85
     """Список для вывода экземпляров """
     all = []
 
-    def __init__(self, name, price, amount):
+    def __init__(self, name: str, price: int, amount: int):
         """инициализация имени, цены, количества"""
-        self.name = name
+        self.__name = name
         self.price = price
         self.amount = amount
         self.all.append(self)
@@ -14,12 +16,54 @@ class Item:
         self.with_discount = 0
         self.total_price = 0
 
-    def apply_discount(self):
+    @property
+    def name(self) -> str:
+        """Геттер возвращающий название товара """
+        return self.__name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        """
+        Сеттер, устанавливающий название товара длинной не больше 10 символов
+        """
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise Exception('Длина наименования товара превышает 10 символов.')
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """ Метод получает данные из файла и превращает в атрибуты"""
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            for line in reader:
+                items = Item(line['name'], line['price'], line['quantity'])
+
+    @staticmethod
+    def is_integer(nubmer: int) -> bool:
+        """проверяет является ли число целым"""
+        isInt = float(nubmer).is_integer()
+        return isInt
+
+    def apply_discount(self) -> int:
         """применение скидки"""
         self.price = self.price * self.pay_rate
         return self.price
 
-    def calculate_total_price(self):
+    def calculate_total_price(self) -> int:
         """подсчет полной цены товара"""
         self.total_price = self.price * self.amount
         return self.total_price
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/test_main.py
+++ test/test_main.py
@@ -23,3 +23,26 @@ def test_apply_discount(item):
 
 def test_calculate_total_price(item):
     assert item.calculate_total_price() == 100000
+
+
+def test_name(item):
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+    with pytest.raises(Exception):
+        item.name('Длина наименования товара превышает 10 символов.')
+
+
+
+def test_instantiate_from_csv(item):
+    assert len(item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'тест'
+
+
+def test_is_integer(item):
+    assert item.is_integer(5) == True
+    assert item.is_integer(5.0) == True
+    assert item.is_integer(5.5) == False
+
