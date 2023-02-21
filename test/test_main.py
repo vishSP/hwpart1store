c3852eada542cb7778298bb436780f18c776d08f
vishnSP <hihi64741@gmail.com>
2023-02-21 20:22:04 +0400

    добавил репр, стр и тесты к ним

--- test/test_main.py
+++ test/test_main.py
@@ -9,19 +9,25 @@ def item():
     return Item(name='тест', price=20000, amount=5)
 
 
-"""проверяет скидку"""
+def test_repr(item):
+    """Проверяет маг метод repr """
+    assert item.__repr__() == "Item('тест',  20000,  5)"
+
+
+def test_str(item):
+    """Проверяет метод str """
+    assert item.__str__() == "тест"
 
 
 def test_apply_discount(item):
+    """проверяет скидку"""
     item.pay_rate = 0.8
     assert item.apply_discount() == 16000.0
     assert item.price == 16000.0
 
 
-"""Проверяет полную цену (товар*колво товара)"""
-
-
 def test_calculate_total_price(item):
+    """Проверяет полную цену (товар*колво товара)"""
     assert item.calculate_total_price() == 100000
 
 
@@ -34,7 +40,6 @@ def test_name(item):
         item.name('Длина наименования товара превышает 10 символов.')
 
 
-
 def test_instantiate_from_csv(item):
     assert len(item.all) == 5
     item1 = Item.all[0]
@@ -45,4 +50,3 @@ def test_is_integer(item):
     assert item.is_integer(5) == True
     assert item.is_integer(5.0) == True
     assert item.is_integer(5.5) == False
-
